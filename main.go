65177c2ae01c235dac7117d4a8d5e10d0753307c
Lukas <lukasvranic@gmail.com>
2024-12-20 00:38:23 +0100

    change the text

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
+		fmt.Fprint(w, "Haha get ratted")
 	})
 
 	fmt.Println("Serving on port 5555")
